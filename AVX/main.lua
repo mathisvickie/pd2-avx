313bedca1db23f5fc68d7ad4605cd451169c6e16
mathisvickie <60326914+mathisvickie@users.noreply.github.com>
2021-04-30 10:56:51 +0200

    fix syntax

--- AVX/main.lua
+++ AVX/main.lua
@@ -1,30 +1,29 @@
 
-local MessageInGame=function(Text)
-	if managers.hud then managers.hud:show_hint({text=Text}) end
-end
-
-local SpawnVehicle=function(VehiclePath)
+local SpawnVehicle = function(VehiclePath)
 	if not Utils:IsInCustody() then
-		World:spawn_unit(Idstring(VehiclePath),managers.player:player_unit():position(),Rotation(managers.player:player_unit():camera():rotation():yaw(),0.0,0.0))
-		managers.player:warp_to(managers.player:player_unit():position()+Vector3(0.0,0.0,200.0),managers.player:player_unit():camera():rotation())
+		
+		World:spawn_unit(Idstring(VehiclePath), managers.player:player_unit():position(), Rotation(managers.player:player_unit():camera():rotation():yaw(), 0.0, 0.0))
+		managers.player:warp_to(managers.player:player_unit():position() + Vector3(0.0, 0.0, 200.0), managers.player:player_unit():camera():rotation())
 	end
 end
 
-local DeactivateUnitBrain=function()
-	for u_key, u_data in pairs( managers.enemy:all_enemies() ) do
-		u_data.unit:brain():set_active( false )
-	end
-	for u_key, u_data in pairs( managers.enemy:all_civilians() ) do
-		u_data.unit:brain():set_active( false )
-	end
+local DeactivateUnitBrain = function()
+	
+	for u_key, u_data in pairs(managers.enemy:all_enemies()) do u_data.unit:brain():set_active(false) end
+	for u_key, u_data in pairs(managers.enemy:all_civilians()) do u_data.unit:brain():set_active(false) end
 end
 
-local DeleteBorders=function()
+local ReactivateUnitBrain = function()
+	
+	for u_key, u_data in pairs(managers.enemy:all_enemies()) do u_data.unit:brain():set_active(true) end
+	for u_key, u_data in pairs(managers.enemy:all_civilians()) do u_data.unit:brain():set_active(true) end
+end
+
+local DeleteInvisibleBorders = function()
 	local net_session = managers.network:session()
-
+	
 	if(net_session) then
-		local send_to_peers = net_session.send_to_peers
-
+		
 		local CollisionData = {
 			["673ea142d68175df"] = true,
 			["86efb80bf784046f"] = true,
@@ -43,14 +42,22 @@ local DeleteBorders=function()
 			["276de19dc5541f30"] = true,
 			["6cdb4f6f58ec4fa8"] = true
 		}
-		for _,unit in pairs(World:find_units_quick("all", 1)) do
+		
+		for _, unit in pairs(World:find_units_quick("all", 1)) do
+			
 			if CollisionData[unit:name():key()] then
-				send_to_peers(net_session, 'remove_unit', unit)
+				
+				net_session.send_to_peers(net_session, 'remove_unit', unit)
 				unit:set_slot(0)
 			end
 		end  
 	end
+end
 
+local FixRestartButton = function()
+	
+	MenuCallbackHandler.singleplayer_restart = function() return true end
+	JobManager.is_current_job_professional = function() return false end
 end
 
 local StartHelicopterMOD=function(TempHelicopterUnitName)
@@ -72,33 +79,34 @@ local StartHelicopterMOD=function(TempHelicopterUnitName)
 end
 
 if LuaNetworking:IsHost() then
-	local Options={}
+	local Options = {}
 
-	local Vehicles={
-		"Falcogini","units/pd2_dlc_cage/vehicles/fps_vehicle_falcogini_1/fps_vehicle_falcogini_1",
-		"Longfellow","units/pd2_dlc_shoutout_raid/vehicles/fps_vehicle_muscle_1/fps_vehicle_muscle_1",
-		"Forklift","units/pd2_dlc_shoutout_raid/vehicles/fps_vehicle_forklift_1/fps_vehicle_forklift_1",
-		"Boxtruck","units/pd2_dlc_jolly/vehicles/fps_vehicle_box_truck_1/fps_vehicle_box_truck_1",
-		"Rib Boat","units/pd2_dlc_jerry/vehicles/fps_vehicle_boat_rib_1/fps_vehicle_boat_rib_1",
-		"Rusts Bike","units/pd2_dlc_born/vehicles/fps_vehicle_bike_1/fps_vehicle_bike_1",
-		"Bike","units/pd2_dlc_born/vehicles/fps_vehicle_bike_2/fps_vehicle_bike_2",
-		"[BETA] Helicopter","units/payday2/vehicles/air_vehicle_blackhawk/vehicle_blackhawk"
+	local Vehicles = {
+		"Falcogini", "units/pd2_dlc_cage/vehicles/fps_vehicle_falcogini_1/fps_vehicle_falcogini_1",
+		"Longfellow", "units/pd2_dlc_shoutout_raid/vehicles/fps_vehicle_muscle_1/fps_vehicle_muscle_1",
+		"Forklift", "units/pd2_dlc_shoutout_raid/vehicles/fps_vehicle_forklift_1/fps_vehicle_forklift_1",
+		"Boxtruck", "units/pd2_dlc_jolly/vehicles/fps_vehicle_box_truck_1/fps_vehicle_box_truck_1",
+		"Rib Boat", "units/pd2_dlc_jerry/vehicles/fps_vehicle_boat_rib_1/fps_vehicle_boat_rib_1",
+		"Rusts Bike", "units/pd2_dlc_born/vehicles/fps_vehicle_bike_1/fps_vehicle_bike_1",
+		"Bike", "units/pd2_dlc_born/vehicles/fps_vehicle_bike_2/fps_vehicle_bike_2",
+		"[BETA] Helicopter", "units/payday2/vehicles/air_vehicle_blackhawk/vehicle_blackhawk"
 	}
 
-	local Index
-	for Index=1,#Vehicles,2 do
-		if Index+1==#Vehicles then
-			table.insert(Options,{text=Vehicles[Index],callback=StartHelicopterMOD,data=Vehicles[Index+1]})
+	for i = 1, #Vehicles, 2 do
+		if i + 1 == #Vehicles then
+			table.insert(Options, { text = Vehicles[i], callback = StartHelicopterMOD, data = Vehicles[i+1] })
 		else
-			table.insert(Options,{text=Vehicles[Index],callback=SpawnVehicle,data=Vehicles[Index+1]})
+			table.insert(Options, { text = Vehicles[i], callback = SpawnVehicle, data = Vehicles[i+1] })
 		end
 	end
-	--table.insert(Options,{text="[DEBUG] Deactivate unit brain",callback=DeactivateUnitBrain})
-	table.insert(Options,{text="[DEBUG] Delete invisible borders",callback=DeleteBorders})
-	table.insert(Options,{})
-	table.insert(Options,{text="Cancel",is_cancel_button=true})
+	--table.insert(Options, { text = "Deactivate unit brain", callback = DeactivateUnitBrain })
+	--table.insert(Options, { text = "Reactivate unit brain", callback = ReactivateUnitBrain })
+	table.insert(Options, { text = "Delete invisible borders", callback = DeleteInvisibleBorders })
+	--table.insert(Options, { text = "Fix restart button", callback = FixRestartButton })
+	table.insert(Options, {})
+	table.insert(Options, { text = "Cancel", is_cancel_button = true })
 
-	QuickMenu:new("Advanced Vehicle Extension","Select vehicle to spawn:",Options):Show()
+	QuickMenu:new("Advanced Vehicle Extension", "Select vehicle to spawn:", Options):Show()
 else
-	MessageInGame("Vehicles can be spawned only by a host.")
+	if managers.hud then managers.hud:show_hint({ text = "Vehicles can be spawned only by a host." }) end
 end
